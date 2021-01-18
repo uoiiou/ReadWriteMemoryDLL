6a89f3ac50d1c2ce9758d52c5f7a866814f544e5
Vlad <62906324+uoiiou@users.noreply.github.com>
2021-01-18 14:47:46 +0300

    Add files via upload

---
+++ DynamicConsole/DynamicConsole.cpp
@@ -0,0 +1,31 @@
+﻿#include <Windows.h>
+#include <iostream>
+
+typedef void(*FNPTR)(int pid, char* from, char* to);
+
+int main(int argc, char* argv[])
+{
+    HINSTANCE hInstance = LoadLibrary(L"..\\Debug\\ReadWriteMemoryDLL.dll");
+
+    if (hInstance)
+    {
+        FNPTR func = (FNPTR)GetProcAddress(hInstance, "run");
+
+        if (func)
+        {
+            func(atoi(argv[1]), argv[2], argv[3]);
+
+            FreeLibrary(hInstance);
+        }
+        else
+        {
+            printf("Coundn't find the function\n");
+        }
+    }
+    else
+    {
+        printf("Couldn't load the library\n");
+    }
+
+    return 0;
+}
---
+++ InjectConsole/InjectConsole.cpp
@@ -0,0 +1,34 @@
+﻿#include <Windows.h>
+#include <iostream>
+
+#define FILEPATH "D:\\Uni\\5sem\\OSiSP\\LB\\LB3\\ReadWriteMemoryDLL\\Debug\\ReadWriteMemoryDLL.dll"
+#define PROCID 15364
+
+typedef HMODULE(WINAPI* LPLoadLibrary)(LPCSTR);
+
+int main()
+{
+    HMODULE hdll = LoadLibraryA("Kernel32.dll");
+
+    LPLoadLibrary LoadLibraryA = (LPLoadLibrary)GetProcAddress(hdll, "LoadLibraryA");
+
+    HANDLE hProc = OpenProcess(PROCESS_VM_READ | PROCESS_VM_OPERATION | PROCESS_VM_WRITE |
+        PROCESS_CREATE_THREAD | PROCESS_CREATE_PROCESS,
+        FALSE, PROCID);
+
+    LPVOID path = VirtualAllocEx(hProc, NULL, strlen(FILEPATH),
+        MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+
+    int er = WriteProcessMemory(hProc, path, FILEPATH,
+        strlen(FILEPATH), NULL);
+
+    DWORD threadID;
+
+    HANDLE HThread = CreateRemoteThread(hProc, NULL, NULL,
+        (LPTHREAD_START_ROUTINE)LoadLibraryA, (LPVOID)path, NULL, &threadID);
+
+    WaitForSingleObject(HThread, INFINITE);
+    
+    FreeLibrary(hdll);
+    CloseHandle(hProc);
+}
